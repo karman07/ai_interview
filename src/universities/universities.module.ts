19d60f2de9e6be640230faeba189946d241b37cd
Advitya Dua <duaadvitya@gmail.com>
2026-03-23 21:01:59 +0530

    Fixed bugs

--- src/universities/universities.module.ts
+++ src/universities/universities.module.ts
@@ -4,12 +4,16 @@ import { University, UniversitySchema } from './schemas/university.schema';
 import { User, UserSchema } from '../users/schemas/user.schema';
 import { UniversitiesService } from './universities.service';
 import { UniversitiesController } from './universities.controller';
+import { Result, ResultSchema } from '../results/schemas/result.schema';
+import { Resume, ResumeSchema } from '../resume/resume.schema';
 
 @Module({
   imports: [
     MongooseModule.forFeature([
       { name: University.name, schema: UniversitySchema },
       { name: User.name, schema: UserSchema },
+      { name: Result.name, schema: ResultSchema },
+      { name: Resume.name, schema: ResumeSchema },
     ]),
   ],
   providers: [UniversitiesService],
